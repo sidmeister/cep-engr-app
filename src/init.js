b0a41e0e84607e83b6aa3c3f42c6ba8905f0fbc6
Sidmeister <rathisiddharth@gmail.com>
2018-08-08 23:05:57 -0500

    initial commit

--- src/init.js
+++ src/init.js
@@ -1,4 +1,16 @@
 // create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
+let fName="this button represent first name of the user";
+$(document).ready(function(){
+    console.log( "ready" );
+    $("#submitBtn").click(function(e){
+        fName= $('#first_name').val();
+        console.log(fName);
+        alert("hello "+ fName);
+    });  
+    $('#firstNameBtn').click(function(e){
+        //alert(fName);
+        $('#first_name').append(fName);
 
-
+    });  
+});
 // using jQuery and the object above, display the information as the appropriate button is clicked
