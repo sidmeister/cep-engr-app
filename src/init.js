1d927ad096d8016f8cb3a66c08d46b461364bcbb
Sidmeister <rathisiddharth@gmail.com>
2018-08-10 15:32:31 -0500

    Final design done

--- src/init.js
+++ src/init.js
@@ -1,35 +1,75 @@
 // create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
 let person={
-    firstName:'Siddharth',
-    lastName:'Rathi',
-    jobTitle:'Staff Big Data Engineer',
-    _homeOffice:'HomeAway ',
-    moreDetail:'Description needed',
+    _firstName:'Siddharth',
+    _lastName:'Rathi',
+    _jobTitle:'Staff Big Data Engineer',
+    _homeOffice:'HomeAway',
+    _moreDetail:'Description needed',
     
-    //TODO : getter  setter for all
-    //TODO : images for all
+    //TODO: change images for all
+    //TODO: add different text for each button
+    //TODO: Format heading
 
-    sayHello(){
-        return `Hello!!! there I am ${this.firstName}`;
-        // return 'hello '+this.firstName;
+
+  //Getter and setter for _firstName 
+    set firstName(newFirstName){
+        console.log(typeof newFirstName)
+        if(typeof newFirstName === "string"){
+            this._firstName=newFirstName;
+        }else{
+            console.error('Invalid Input');
+        }
     },
-
-
-    //Getter and setter for property 
+    get firstName(){
+        return this._firstName;
+    },
+  //Getter and setter for _lastName 
+    set lastName(newLastName){
+        console.log(typeof newLastName)
+        if(typeof newLastName === "string"){
+            this._lastName=newLastName;
+        }else{
+            console.error('Invalid Input');
+        }
+    },
+    get lastName(){
+        return this._lastName;
+    },
+  //Getter and setter for _jobTitle 
+    set jobTitle(newJobTitle){
+        console.log(typeof newJobTitle)
+        if(typeof newJobTitle === "string"){
+            this._jobTitle=newJobTitle;
+        }else{
+            console.error('Invalid Input');
+        }
+    },
+    get jobTitle(){
+        return this._jobTitle;
+    },
+    //Getter and setter for _homeOffice 
     set homeOffice(newHomeOffice){
-        // TODO: handle null case
-        if(typeof newHomeOffice==='string'){
+        console.log(typeof newHomeOffice)
+        if(typeof newHomeOffice === "string"){
             this._homeOffice=newHomeOffice;
         }else{
-            return 'Invalid Input';
+            console.error('Invalid Input');
         }
     },
     get homeOffice(){
         return this._homeOffice;
     },
-
-    aboutYourself(){
-        let aboutMeTemp;
+    //Getter and setter for _moreDetail 
+    set moreDetail(newMoreDetail){
+        console.log(typeof newMoreDetail)
+        if(typeof newMoreDetail === "string"){
+            this._moreDetail=newMoreDetail;
+        }else{
+            console.error('Invalid Input');
+        }
+    },
+    get moreDetail(){
+        return this._moreDetail;
     }
 };
 
@@ -39,34 +79,33 @@ $(document).ready(function(){
     //hide Description Div and Picture by default
     $('.descBackground, .sideImage').hide();
 
-    //Information to be displayed if first name button is clicked
-    $('#first_name_btn').click(function(e){
+    //common functionality for all buttons
+    $("button").click(function(){
         //show Description Div and Picture by default
         $('.descBackground, .sideImage').show(); 
+        //disable button afterone click
+        $(this).off('click'); 
+    });
+    //Information to be displayed if first name button is clicked
+    $('#first_name_btn').click(function(e){
         //using Jquery append function to display text from Java script method having firstName property
-        $('#detailBox').append('<br/>'+ person.sayHello()); 
+        $('#detailBox').append('<br/> Hello!!! I am '+ person.firstName);
     });
 
     //Information to be displayed if last name button is clicked
     $('#last_name_btn').click(function(e){
-        //show Description Div and Picture by default
-        $('.descBackground, .sideImage').show(); 
         //using Jquery append function to display text from Java script object having lastName property
         $('#detailBox').append('<br/> My Last name is '+person.lastName); 
     });
 
     //Information to be displayed if homeOffice button is clicked
     $('#home_office_btn').click(function(e){
-        //show Description Div and Picture by default
-        $('.descBackground, .sideImage').show(); 
         //using Jquery append function to display text from Java script getter method returning homeOffice property
         $('#detailBox').append('<br/> I work in '+person.homeOffice); 
     });
 
     //Information to be displayed if Job Title button is clicked
     $('#job_title_btn').click(function(e){
-        //show Description Div and Picture by default
-        $('.descBackground, .sideImage').show(); 
         //using Jquery append function to display text from Java script object having jobTitle property
         $('#detailBox').append('<br/> My Job Titile is '+person.jobTitle); 
     });
