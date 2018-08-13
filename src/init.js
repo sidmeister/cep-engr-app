e9bca758dceac5ffa0751521d9f143a59c206116
Sidmeister <rathisiddharth@gmail.com>
2018-08-13 11:38:53 -0500

    added styling

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ let person={
     _lastName:'Rathi',
     _jobTitle:'Staff Big Data Engineer',
     _homeOffice:'HomeAway',
-    _tellMeMore:'I am originally from west part of India. I love Traveling and one of my fond memories is doing a road trip of the west coast with my wife. Some of the key highlights were driving and enjoying the goregous view on CA-1, riding the buggy on the sand dunes in Oregon, hiking down Mount Shashta,spending time on the picturesque Canyon beach, to visiting the space needle. This is me with my wife posing at the Multnomah Falls, Oregon',
+    _tellMeMore:'I am originally from west part of India. I love traveling and one of my fond memories is doing a road trip from London To Scotland with my wife. Some of the key highlights were driving and enjoying the goregous view of the scottish highlands, spending time in the picturesque Edinburgh visiting Edinburgh castle, doing scotch tasting tours, going on a ferry to Loch ness Monster This is me with my wife posing on the ferry to Loch ness Monster',
 
   //Getter  _firstName 
     get firstName(){
@@ -46,39 +46,38 @@ $(document).ready(function(){
     //Information to be displayed if first name button is clicked
     $('#first_name_btn').click(function(e){
         var intro = "<br/>Some facts about the name Siddharth";
-        var urban_dict_fact = "<br/>However urban dictionaly says: An amazing person in general, as well as very kind and understanding. Attracts lots of girls and usually has long eyelashes.Siddharth is a synonym for perfectionist, kind, caring, sophisticated and  a mannered guy but the one who at the same time can be called as ruthless, cruel and a sadist";
-        var mythology_fact = "<br/>According to Sanskrit Siddharth is a name derived from two Sanskrit words:\"Siddha\" - meaning accomplished; \"artha\" - meaning goal";
-        var endIntro = "I will let you to decide"
+        var urban_dict_fact = "<br/><h6><i><b>Urban dictionary says</b></i></h6>An amazing person in general, as well as very kind and understanding. Attracts lots of girls and usually has long eyelashes.Siddharth is a synonym for perfectionist, kind, caring, sophisticated and  a mannered guy but the one who at the same time can be called as ruthless, cruel and a sadist";
+        var mythology_fact = "<br/><h6><i><b>Mythology says</b></i></h6> Siddharth is a name derived from two Sanskrit words:\"Siddha\" - meaning accomplished; \"artha\" - meaning goal";
+        var endIntro = "<br/><em>I will let you decide which is more accurate once you get to know me</em>"
         miscDetail = intro +  mythology_fact +  urban_dict_fact + endIntro;
         //using Jquery text function to display text from Java script method having firstName property
-        displayDetailBoxHelper('Howdy! My name is ' + person.firstName + miscDetail);
+        displayDetailBoxHelper('<strong>Howdy! My name is ' + person.firstName + '</strong>' + miscDetail);
         $('#imageToBeDisplayed').attr("src","../img/first_name.jpg");
     });
 
     //Information to be displayed if last name button is clicked
     $('#last_name_btn').click(function(e){
         //using Jquery text function to display text from Java script object having lastName property
-        displayDetailBoxHelper('My Last name is ' + person.lastName + '.No fun facts here its just a family name');
-        $('#imageToBeDisplayed').attr("src","../img/last_name.jpg");
+        displayDetailBoxHelper('<strong>My last name is ' + person.lastName + '</strong> <br/> <em>No fun facts here its just a family name</em>');
+        // $('#imageToBeDisplayed').attr("src","../img/last_name.jpg");
     });
 
     //Information to be displayed if homeOffice button is clicked
     $('#home_office_btn').click(function(e){
         var homeaway_details = ".It is a vacation rental marketplace with more than 2,000,000 vacation rentals in 190 countries listed on its website. It operates through 50 websites in 23 languages.The company offers rentals for families and groups in cabins, condos, castles, villas, barns and farm houses. Founded in February 2005 and headquartered in Austin, the company became a publicly traded company in 2011. Expedia Inc.acquired HomeAway on December 15, 2015. <br/> Source : <a href='https://en.wikipedia.org/wiki/HomeAway' target ='_blank'> https://en.wikipedia.org/wiki/HomeAway</a>";
         //using Jquery text function to display text from Java script getter method returning homeOffice property
-        displayDetailBoxHelper('I work at ' + person.homeOffice + homeaway_details);
+        displayDetailBoxHelper('<strong>I work at ' + person.homeOffice + '</strong>' + homeaway_details);
         $('.imageBox').show();
         $('#imageToBeDisplayed').attr("src","../img/homeaway.jpg").attr("style","height:40%;");
-       // $('#imageToBeDisplayed').attr("src","../img/homeaway.jpg");
 
     });
 
     //Information to be displayed if Job Title button is clicked
     $('#job_title_btn').click(function(e){
-        var jobDesc = "<br/>Big data engineers develop, maintain, test and evaluate big data solutions within organisations. Most of the time they are also involved in the design of big data solutions, because of the experience they have in Apache Spark or Hadoop based technologies such as MapReduce, Hive MongoDB or Cassandra";
-        var myDuties = '<br/>My primary responsibility <ul> <li>Performing data cleansing enrichment,data conversion through data de-duplication and construction (ETL) </li> <li> I develop, test and maintain big data pipelines within Homeaway.Most of my time is spent in transforming, wrangling, and manipulating data using Spark and Hadoop based technologies such as Map - Reduce, Presto, Hive</li></ul>'
+        var jobDesc = "<br/><h6><i><b> What to they do</b></i></h6>Big data engineers develop, maintain, test and evaluate big data solutions within organisations. Most of the time they are also involved in the design of big data solutions, because of the experience they have in Apache Spark or Hadoop based technologies such as MapReduce, Hive MongoDB or Cassandra";
+        var myDuties = '<br/><h6><i><b>What do I do</b></i></h6><ul> <li>Perform data cleansing enrichment,data conversion through data de-duplication and construction (ETL) </li> <li> Develop, test and maintain big data pipelines within Homeaway.Most of my time is spent in transforming, wrangling, and manipulating data using Spark and Hadoop based technologies such as Map - Reduce, Presto, Hive</li></ul>'
         //using Jquery text function to display text from Java script object having jobTitle property
-        displayDetailBoxHelper('My Job Title is ' + person.jobTitle + jobDesc + myDuties);
+        displayDetailBoxHelper('<strong>My Job Title is ' + person.jobTitle + '</strong>' + jobDesc + myDuties);
         // $('#imageToBeDisplayed').attr("src","../img/big-data.jpg").attr("style","height:60%;");
         $('#imageToBeDisplayed').attr("src","../img/big-data.jpg");
 
@@ -91,6 +90,6 @@ $(document).ready(function(){
         //using Jquery text function to display text from Java script method which is having firstName property
         displayDetailBoxHelper(person.tellMeMore);
         // $('#imageToBeDisplayed').attr("src","../img/tellmemore.jpg");
-        $('#imageToBeDisplayed').attr("src","../img/tellmemore_1.png");
+        $('#imageToBeDisplayed').attr("src","../img/tellmemore.jpg");
     });
 });
